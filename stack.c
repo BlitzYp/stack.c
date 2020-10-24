9456f26f33c1bb46c87c74d8929d780f86e1e009
BlitzYp <63169496+BlitzYp@users.noreply.github.com>
2020-10-24 22:14:48 +0300

    Changed the typedef names

--- stack.c
+++ stack.c
@@ -5,39 +5,39 @@
 typedef struct Node
 {
     char *value;
-    struct Node *next;
-} node;
+    struct node *next;
+} node_t;
 
 typedef struct Stack
 {
-    node *bottom;
-    node *top;
+    node_t *bottom;
+    node_t *top;
     size_t length;
-} stack;
+} stack_t;
 
-stack createStack()
+stack_t createStack()
 {
-    stack s = {NULL, NULL, 0};
+    stack_t s = {NULL, NULL, 0};
     return s;
 }
 
-void push(stack *s, char *value);
-void pop(stack *s);
-char *peek(stack *s);
+void push(stack_t *s, char *value);
+void pop(stack_t *s);
+char *peek(stack_t *s);
 
 int main(void)
 {
-  stack s = createStack();
-  push(&s, "HI");
-  push(&s, "HI2");
-  pop(&s);
-  char* res = peek(&s);
-  printf("%s\n", res);
+    stack_t s = createStack();
+    push(&s, "HI");
+    push(&s, "HI2");
+    pop(&s);
+    char *res = peek(&s);
+    printf("%s\n", res);
 }
 
-void push(stack *s, char *value)
+void push(stack_t *s, char *value)
 {
-    node *newNode = malloc(sizeof(node));
+    node_t *newNode = malloc(sizeof(node_t));
     char *memorySpace = malloc(strlen(value) + 1);
     memorySpace = value;
     newNode->value = memorySpace;
@@ -49,14 +49,15 @@ void push(stack *s, char *value)
         return;
     }
     s->top = newNode;
-    s->length ++;
+    s->length++;
     return;
 }
 
-void pop(stack *s) 
+void pop(stack_t *s)
 {
-    if (s->length >= 0) {
-        node* hasToGo = s->top;
+    if (s->length >= 0)
+    {
+        node_t *hasToGo = s->top;
         free(s->top->value);
         s->top = s->top->next;
         free(hasToGo);
@@ -66,7 +67,7 @@ void pop(stack *s)
     return;
 }
 
-char *peek(stack *s)
+char *peek(stack_t *s)
 {
     if (s->top)
     {
